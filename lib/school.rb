030c54de15a77f38a1583565a20f557bc3f788f0
Abagail Hudson <abagailanne@gmail.com>
2019-05-19 18:10:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,8 @@ class School
     @roster
   end
 
-  def add_student(name, grade) //grade is like 6th 7th 8th
-    roster[name] = []
+  def add_student(name, grade) #grade is like 6th 7th 8th
+    roster[grade] = [] unless roster[grade]
+    roster[grade] << name
   end
 end
